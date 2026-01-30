a51aa1ce8e2088780e2787dde07a79de4b9439f3
vijeth44-pro <captainmncaptain@gmail.com>
2026-01-30 16:26:55 +0530

    first commit

---
+++ ecom/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Box, Typography} from '@mui/material'
+
+export default function 
+() {
+  return (
+    <div>
+        <Box sx={{border:'2px solid black', borderRadius:5,backgroundColor:'grey'}}>
+            <Typography sx={{textAlign:'center', fontWeight:'bold', padding:'10px'}}>
+                Designed by ChatGPT 
+            </Typography>
+        </Box>
+    </div>
+  )
+}
---
+++ ecom/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ResponsiveAppBar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <div>
+      <ResponsiveAppBar/>
+      <Footer/>
+    </div>
+  )
+}
