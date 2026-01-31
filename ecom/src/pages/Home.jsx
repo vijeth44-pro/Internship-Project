f57ed6d133f84b45d7ef0d659b24af6abcdca28d
vijeth44-pro <captainmncaptain@gmail.com>
2026-01-31 19:40:01 +0530

    implemented useState and useEffect to change to change number and print in console

--- ecom/src/pages/Home.jsx
+++ ecom/src/pages/Home.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import ResponsiveAppBar from '../components/Navbar'
 import Footer from '../components/Footer'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 export default function Home() {
+  const [number, setNumber] = useState(0)
+
+  const changeNumber = () => {
+    console.log("Number changed");
+  }
+
+  // useEffect(()=> {
+    //} ,[dependency array])}
+
+  useEffect(()=>{
+    changeNumber();
+  },[number]) // runs only once when component mounts 
+
   return (
     <div>
-      <ResponsiveAppBar/>
-      <Footer/>
+      This is Number : {number}
+      <button onClick={() => setNumber(number+1)}>+</button>
+      {/* <button onClick={() => setNumber(number-1)}>-</button> */}
     </div>
   )
 }
