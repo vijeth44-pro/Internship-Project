cb1ab015a7f11aad721b4ddc1809e8aa37322b2d
vijeth44-pro <captainmncaptain@gmail.com>
2026-01-31 19:41:12 +0530

    called navbar,footer,products,about inside app and used browserRouter for routing

--- ecom/src/App.jsx
+++ ecom/src/App.jsx
@@ -4,6 +4,10 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
+import ResponsiveAppBar from './components/Navbar'
+import Footer from './components/Footer'
+import Products from './pages/Products'
+import About from './pages/About'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,9 +15,14 @@ function App() {
   return (
     <>
       <BrowserRouter>
+      <ResponsiveAppBar/>
+      {/* always keep navbar inside browser router */}
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/products' element ={<Products/>}/>
+        <Route path='/about' element ={<About/>}/>
       </Routes>
+      <Footer/>
       </BrowserRouter>
     </>
   )
