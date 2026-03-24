87af77778b6ba0fc6ab73e07e31354496b7d9579
vijeth44-pro <captainmncaptain@gmail.com@users.noreply.github.com>
2026-03-24 21:41:56 +0530

    Basic changes in navbar

--- ecom/src/components/Navbar.jsx
+++ ecom/src/components/Navbar.jsx
@@ -59,7 +59,7 @@ function ResponsiveAppBar() {
               textDecoration: "none",
             }}
           >
-            My Shop
+            Rai Angadi
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
